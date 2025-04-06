d49604c5c23cdad1e0e4848995879aa41e72e774
lucas2mz <lucasandrada2606@gmail.com>
2025-04-06 18:25:29 -0300

    Task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+const cleanSet = (set, startString) => {
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+  const filtered = [...set]
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+  return filtered.join('-');
+};
+
+export default cleanSet;
