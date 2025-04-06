f9b852f788a0c1e2ecaeacb7ab278eead01d322e
lucas2mz <lucasandrada2606@gmail.com>
2025-04-06 18:32:25 -0300

    Task 10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const [item, quantity] of map.entries()) {
+    if (quantity === 1) {
+      map.set(item, 100);
+    }
+  }
+  return map;
+};
