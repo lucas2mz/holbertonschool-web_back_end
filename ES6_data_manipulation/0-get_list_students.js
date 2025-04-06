77181dd03ff97f958d57f16f76542b5c46490c63
lucas2mz <lucasandrada2606@gmail.com>
2025-04-06 18:36:40 -0300

    Task 10

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,11 @@
-const getListStudents = () => [
-  {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  },
-  {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  },
-  {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  },
-];
+function getListStudents() {
+  const list = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return list;
+}
+
 export default getListStudents;
