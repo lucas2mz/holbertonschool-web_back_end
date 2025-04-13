c5496ec6fd65b50bad43c1129e66c1e3e4f40dc7
lucas2mz <lucasandrada2606@gmail.com>
2025-04-13 17:14:37 -0300

    Task 2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'success' }, console.log('Got a response from the API'));
+    } else {
+      reject(Error(), console.log('Got a response from the API'));
+    }
+  });
+}
