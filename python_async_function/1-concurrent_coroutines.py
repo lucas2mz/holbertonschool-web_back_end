25a0b0a81b4b44ed175c0c0f69a7e562ac1edaee
lucas2mz <lucasandrada2606@gmail.com>
2025-04-27 17:11:57 -0300

    Task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Python3 module"""
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+import asyncio
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Return a list of all the delays """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
