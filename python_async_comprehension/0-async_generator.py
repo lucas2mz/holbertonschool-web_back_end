d9fd48576b5f86becc1482952b835396b1474564
lucas2mz <lucasandrada2606@gmail.com>
2025-04-30 10:06:39 -0300

    Task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,12 +2,12 @@
 """Module commented"""
 
 
+from typing import Generator
 import asyncio
 import random
-from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Returns a yield with a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
