a2cc9264b3832d028e9a0b7b1818ed3911a43e4a
lucas2mz <lucasandrada2606@gmail.com>
2025-05-03 18:47:48 -0300

    Task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -36,13 +36,13 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Return the appropriate page of the dataset"""
-        assert(isinstance(page, int) and page > 0)
-        assert(isinstance(page_size, int) and page_size > 0)
+        assert (isinstance(page, int) and page > 0)
+        assert (isinstance(page_size, int) and page_size > 0)
 
         start, end = index_range(page, page_size)
         dataset = self.dataset()
 
         if start >= len(dataset):
             return []
-        
+
         return dataset[start:end]
