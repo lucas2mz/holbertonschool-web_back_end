556ae9c31a3b6f7cc45cc2cf6e1f6e53bd08168e
lucas2mz <lucasandrada2606@gmail.com>
2025-05-03 19:48:31 -0300

    Task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -50,7 +50,7 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """Returns a dictionary with the hypermedia data"""
-        data = self.get_page(page,page_size)
+        data = self.get_page(page, page_size)
         total_items = len(self.dataset())
         total_pages = math.ceil(total_items / page_size)
 
