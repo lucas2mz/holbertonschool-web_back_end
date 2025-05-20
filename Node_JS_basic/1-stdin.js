4ed0949b9631605d1b3d93bf7f7899e2f6dd43ae
lucas2mz <lucasandrada2606@gmail.com>
2025-05-20 11:23:49 -0300

    Task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  const message = `Your name is: ${name}\n`;
-
-  process.stdout.write(message);
-
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
