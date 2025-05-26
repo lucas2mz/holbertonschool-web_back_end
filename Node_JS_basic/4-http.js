8b928e936485d2dfc38b44e0ef91d6c29b0233de
lucas2mz <lucasandrada2606@gmail.com>
2025-05-25 21:45:39 -0300

    Task 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,13 +1,11 @@
-import http from 'http';
+const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  const port = 1245;
-  app.listen(port, () => {
-    console.log(`El servidor está escuchando en el puerto ${port}`);
-  });
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
+
+app.listen(1245);
 
 module.exports = app;
